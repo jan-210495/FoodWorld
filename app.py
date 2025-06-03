4dd60e95f5ce35965e07d17ce09a9e6aa0990d0b
jan-210495 <150586363+jan-210495@users.noreply.github.com>
2025-06-03 01:34:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    load_dotenv()
+    CORS(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+    # Load environment variables
+    load_dotenv()
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    CORS(app)
+    migrate.init_app(app, db)
 
-    # Register blueprints
+    # Register Blueprints
     from resources.recipe_routes import recipe_blueprint
-    from resources.user_routes import user_blueprint
+    app.register_blueprint(recipe_blueprint, url_prefix='/recipes')
 
-    app.register_blueprint(recipe_blueprint, url_prefix="/recipe")
-    app.register_blueprint(user_blueprint, url_prefix="/user")
+    from resources.user_routes import user_blueprint
+    app.register_blueprint(user_blueprint, url_prefix='/users')
 
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
