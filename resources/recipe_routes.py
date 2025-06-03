ee1620f426ff0f2b7998c2721dc7321c0c4fd708
jan-210495 <150586363+jan-210495@users.noreply.github.com>
2025-06-03 02:58:33 -0700

    Update recipe_routes.py

--- resources/recipe_routes.py
+++ resources/recipe_routes.py
@@ -1,1 +1,14 @@
+from utils.openai_helper import generate_recipe_recommendation
+
+@recipe_bp.route('/recommend', methods=['POST'])
+def recommend_recipe():
+    data = request.get_json()
+    ingredients = data.get('ingredients', [])
+    num_people = data.get('num_people', 1)
+
+    if not ingredients or not isinstance(ingredients, list):
+        return jsonify({"error": "Ingredients must be a list"}), 400
+
+    suggestions = generate_recipe_recommendation(ingredients, num_people)
+    return jsonify({"suggestions": suggestions})
 
