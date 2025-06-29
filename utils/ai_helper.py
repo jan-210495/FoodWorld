aa6b7a809e01e994bb40653c6479fd614fe9ac66
jan-210495 <150586363+jan-210495@users.noreply.github.com>
2025-06-29 13:07:28 -0700

    Update ai_helper.py

--- utils/ai_helper.py
+++ utils/ai_helper.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 HF_API_URL = "https://api-inference.huggingface.co/models/gpt2"
-HF_API_KEY = os.getenv("HF_API_KEY")
+HF_API_KEY = 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
 
 def generate_recipe(ingredients, servings):
     prompt = (
